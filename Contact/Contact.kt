7fa7fa45fb5b790375279bed6e66ad4b0f35cae1
Vibe167 <164038002+Vibe167@users.noreply.github.com>
2026-04-20 15:22:50 +0530

    Create Contact.kt
    Added types of attributes.

---
+++ Contact/Contact.kt
@@ -0,0 +1,11 @@
+// Contact.kt
+package com.example.contactapp
+
+data class Contact(
+    val name: String,
+    val role: String,
+    val phone: String,
+    val email: String,
+    val birthday: String,
+    val imageResId: Int      
+)
